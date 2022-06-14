e78bd79db5bd79297a482a1d32c901f1aad4a760
ilus <ilusder@gmail.com>
2022-06-14 22:40:04 +0300

    Create script

--- books.py
+++ books.py
@@ -1,8 +1,36 @@
+import time
+
 import requests as req
+from config import API_ID
+import csv
+import json
+
+books = []
+
+with open('books.list.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        place = row['place'] if row['place'] != "" else place
+        row_n = row['row'] if row['row'] != "" else row_n
+        book = {'place': place,
+                'row': row_n,
+                'ibsn': row['ibsn']}
 
 
+        h = {'Authorization': API_ID}
+        resp = req.get("https://api2.isbndb.com/book/{}".format(row['ibsn']), headers=h)
+        resp_json = resp.json()
+        book["authors"] = \
+            resp_json['book']['authors'][0] if 'book' in resp_json \
+                                               and 'authors' in resp_json['book'] \
+                                               and len(resp_json['book']['authors']) else ""
+        book["title"] = resp_json['book']['title'] if 'book' in resp_json else ""
+        books.append(book)
+        time.sleep(1)
 
-isbn_number = '9785171184841'
-h = {'Authorization': API_ID}
-resp = req.get("https://api2.isbndb.com/book/{}".format(isbn_number), headers=h)
-print(resp.json())
+with open('out.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=book.keys())
+    writer.writeheader()
+    writer.writerows(books)
+
+
